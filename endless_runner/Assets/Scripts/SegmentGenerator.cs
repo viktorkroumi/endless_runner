a8f20c302f0754986899d80888338c0bdbed548b
viktorkroumi <viktor.mrakota@student.gymspit.cz>
2025-02-24 22:55:04 +0100

    24.02.02

--- endless_runner/Assets/Scripts/SegmentGenerator.cs
+++ endless_runner/Assets/Scripts/SegmentGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SegmentGenerator : MonoBehaviour
 {
     public GameObject[] segment;
-    private int zPos = 50;
+    private int zPosition = 50;
     private bool creatingSegment = false;
     private int segmentNum;
 
@@ -23,8 +23,8 @@ public class SegmentGenerator : MonoBehaviour
     IEnumerator SegmentGen()
     {
         segmentNum = Random.Range(0, 5);
-        Instantiate(segment[segmentNum], new Vector3(0, 0, zPos), Quaternion.identity);
-        zPos += 50;
+        Instantiate(segment[segmentNum], new Vector3(0, 0, zPosition), Quaternion.identity);
+        zPosition += 50;
         yield return new WaitForSeconds(3);
         creatingSegment = false;
     }
